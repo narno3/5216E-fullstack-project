2215f515f0ff6eb1734baed14524892594373696
Jules BERTRAND <jules.bertrand@edu.esiee.fr>
2024-12-13 16:32:41 +0100

    Added login tokens

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+import datetime
+from flask import Flask, request
+from flask_cors import CORS
+import hashlib
+import jwt
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
@@ -10,6 +15,39 @@ def hello_world():
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
+
+
+secret = "Groupe Henri Victor"
+expiration_in_seconds = 3600
+
+
+def build_token():
+    """
+    Generates the Auth Token
+    :return: string
+    """
+    try:
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=expiration_in_seconds),
+            'iat': datetime.datetime.utcnow(),
+            'sub': 'quiz-app-admin'
+        }
+        return jwt.encode(
+            payload,
+            secret,
+            algorithm="HS256"
+        )
+    except Exception as e:
+        return e
+    
+@app.route('/login', methods=['POST'])
+def getPassword():
+	payload = request.get_json()
+	if payload['password'] == 'flask2023':
+		access_token = {}
+		access_token['token'] = build_token()
+		return access_token
+	return 'Unauthorized', 401
 
 if __name__ == "__main__":
     app.run()
