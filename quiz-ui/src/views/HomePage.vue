331d60eff1b61781b4f2de201257edac005050ae
narno3 <nathan.arnould@edu.esiee.fr>
2024-12-17 16:38:06 +0100

    composant homepage -- wip

--- quiz-ui/src/views/HomePage.vue
+++ quiz-ui/src/views/HomePage.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { ref, onMounted } from 'vue';
-// import quizApiService from "@/services/QuizApiService";
+import quizApiService from '@/services/QuizApiService';
 
+const registeredScores = ref([]);
 onMounted(async () => {
   console.log('Home page mounted');
+  quizApiService();
 });
 </script>
-
 <template>
   <h1>Home page</h1>
 </template>
