0a8681ecf10154b38f41a8641c1857ac631c9769
narno3 <nathan.arnould@edu.esiee.fr>
2024-12-17 16:14:25 +0100

    homepage.vue

---
+++ quiz-ui/src/views/HomePage.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+// import quizApiService from "@/services/QuizApiService";
+
+onMounted(async () => {
+  console.log('Home page mounted');
+});
+</script>
+
+<template>
+  <h1>Home page</h1>
+</template>
